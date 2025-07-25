63f20b3e71b729c816e1db23ecb8bb47a4794910
CHEGEBB <chegephil24@gmail.com>
2025-07-25 20:58:21 +0300

    Update background image in SavingsScreen to use pattern1.png

--- dailydime/lib/screens/savings/savings_screen.dart
+++ dailydime/lib/screens/savings/savings_screen.dart
@@ -193,7 +193,7 @@ class _SavingsScreenState extends State<SavingsScreen> with SingleTickerProvider
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(24),
                 image: const DecorationImage(
-                  image: AssetImage('assets/images/pattern5.png'),
+                  image: AssetImage('assets/images/pattern1.png'),
                   fit: BoxFit.cover,
                   opacity: 0.4,
                 ),
